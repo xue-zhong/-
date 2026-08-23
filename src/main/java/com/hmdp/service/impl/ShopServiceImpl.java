ce9bf3382b4a44eb571a624374185a7df06df1c6
xue-zhong <m19273607093@163.com>
2026-08-23 15:45:39 +0800

    缓存穿透解决，并使用互斥锁解决缓存击穿

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,24 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -17,4 +31,112 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryById(Long id) throws InterruptedException {
+        Shop shop=queryWithMutex(id);
+        if(shop==null){
+            return Result.fail("店铺不存在");
+        }
+        return Result.ok(shop);
+    }
+
+    @Override
+    @Transactional
+    public Result updateId(Shop shop) {
+        Long id= shop.getId();
+        if(id==null){
+            return Result.fail("店铺id不能为空");
+        }
+        updateById(shop);
+        stringRedisTemplate.delete(CACHE_SHOP_KEY+id);
+        return Result.ok();
+
+    }
+
+
+    private boolean tryLock(String key){
+        Boolean flag=stringRedisTemplate.opsForValue().setIfAbsent(key,"1",10,TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(flag);
+    }
+    private void unlock(String key){
+        stringRedisTemplate.delete(key);
+    }
+
+//    缓存穿透
+    public Shop queryWithPassThrough(Long id){
+        String key=CACHE_SHOP_KEY+id;
+        //从Redis中查询商铺
+        String shopJson= stringRedisTemplate.opsForValue().get(key);
+        //判断商铺Redis中是否存在
+        if(StrUtil.isNotBlank(shopJson)){
+            //存在，直接返回
+            Shop shop=JSONUtil.toBean(shopJson, Shop.class);
+            return shop;
+        }
+        if(shopJson!=null){
+            return null;
+        }
+
+        //Mybatis-puls的内部类，直接调用需要在实体类添加@TableName("tb_shop")来指定操作的哪个表
+        //不存在，查询数据库
+        Shop shop=getById(id);
+        if(shop==null){
+            //数据库也不存在，把空值写入Redis，防止缓存穿透
+            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL, TimeUnit.MINUTES);
+            return null;
+        }
+        //数据库存在，把数据写入缓存
+        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        //返回商铺信息
+        return shop;
+    }
+        //互斥锁解决缓存击穿
+    public Shop queryWithMutex(Long id) throws InterruptedException {
+        String LockKey=LOCK_SHOP_KEY+id;
+        String key=CACHE_SHOP_KEY+id;
+        //从Redis中查询商铺
+        String shopJson= stringRedisTemplate.opsForValue().get(key);
+        //判断商铺Redis中是否存在
+        if(StrUtil.isNotBlank(shopJson)){
+            //存在，直接返回
+            Shop shop=JSONUtil.toBean(shopJson, Shop.class);
+            return shop;
+        }
+        if(shopJson!=null){
+            return null;
+        }
+        Shop shop=null;
+        try {
+            //不存在，获取互斥锁
+            Boolean lock= tryLock(LockKey);
+            //锁不存在，休眠一段时间后重试
+            if(!lock){
+                Thread.sleep(50);
+                return queryWithMutex(id);
+            }
+            //Mybatis-puls的内部类，直接调用需要在实体类添加@TableName("tb_shop")来指定操作的哪个表
+            //锁存在，查询数据库
+            Thread.sleep(200);
+            shop=getById(id);
+            if(shop==null){
+                //数据库也不存在，把空值写入Redis，防止缓存穿透
+                stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL, TimeUnit.MINUTES);
+                return null;
+            }
+
+            //数据库存在，把数据写入缓存
+            stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
+
+
+        } finally {
+            //释放互斥锁
+            unlock(LockKey);
+
+        }
+        //返回商铺信息
+        return shop;
+    }
 }
--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -20,36 +20,15 @@ import static com.hmdp.utils.RedisConstants.LOGIN_USER_TTL;
 import static net.sf.jsqlparser.util.validation.metadata.NamedObject.user;
 
 public class LoginInterceptor implements HandlerInterceptor {
-    private StringRedisTemplate stringRedisTemplate;
-    public LoginInterceptor (StringRedisTemplate stringRedisTemplate){
-        this.stringRedisTemplate=stringRedisTemplate;
-    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        HttpSession session=request.getSession();
-        String token= request.getHeader("authorization");
-        if(StrUtil.isBlank(token)){
+        if(UserHolder.getUser()==null){
             response.setStatus(401);
             return false;
         }
-//        UserDTO user = (UserDTO) session.getAttribute("user");
-//        为空则会返回 null
-        Map<Object,Object> userMap=stringRedisTemplate.opsForHash().entries(LOGIN_USER_KEY+token);
-        if(userMap.isEmpty()){
-            response.setStatus(401);
-            return  false;
-        }
-//        转换为UserDto对象，false是不忽略转换产生的异常
-        UserDTO user= BeanUtil.fillBeanWithMap(userMap,new UserDTO(),false);
-
-        UserHolder.saveUser(user);
-//        刷新key的有效时长
-        stringRedisTemplate.expire(LOGIN_USER_KEY+token,LOGIN_USER_TTL, TimeUnit.MINUTES);
         return true;
     }
 
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        UserHolder.removeUser();
-    }
+
 }
